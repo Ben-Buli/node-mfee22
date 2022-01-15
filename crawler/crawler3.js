6339cc6236d1348fba26179b8bead0d0401b0aa2
Ben-Buli <84383897+Ben-Buli@users.noreply.github.com>
2022-01-15 20:26:50 +0800

    20220115 course files

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,14 +1,15 @@
 const axios = require("axios");
 const { readFile } = require("fs/promises");
+const moment = require("moment");//引用
 
 
 
 (async () => {
 try{
-    let  stockNo = await readFile("stocktxt","utf-8")
-    let queryDate = "20220115";
+    let  stockNo = await readFile("stock.txt","utf-8")
+    let queryDate = moment().format("YYYYMMDD"); // 自動用今天的日期//"now";///TODO:改成自動用今天的日期
 
-    // TODO: 從 stock.txt 中讀出檔案代碼
+    // DONE: 從 stock.txt 中讀出檔案代碼
 
     let response = await axios.get(
       "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
